9a7b31fde0c9a89b6de1450cfe1ac3c5b5d906f0
Zech Drinkhall <zechdrink@aol.com>
2019-05-28 14:34:24 -0700

    finished files 00 - 06

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+print('Since %s I fed them to %s sharks for %.2f seconds' % (z, x, y))
 # Use the 'format' string method to print the same thing
-
+print('Since {} I fed them to {} sharks for {} seconds'.format(z,x,round(y, 2)))
 # Finally, print the same thing using an f-string
+print(f'Since {z} I fed them to {x} sharks for {round(y, 2)} seconds')
--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,33 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+for p in y:
+    x.append(p) 
+    print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE 
-
+print(len(x))
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+result = []
+for i in x:
+    result.append(i*1000)
+    i = result
+
+print(i)
